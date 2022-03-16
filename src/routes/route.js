4c89876655058b22cfc986c71b2fce2492b2705f
pawankanyal1 <pawaneshkanyal1@gmail.com>
2022-03-17 00:59:42 +0530

    bloging sitr Project Solution Completed

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,16 +4,16 @@ const router = express.Router();
 
 const authorController = require('../controllers/authorController');
 const blogController = require('../controllers/blogController')
-const authorAuth = require('../middlewares/authorAuth')
+const Auth = require('../middlewares/Auth')
 // Author routes
 router.post('/authors', authorController.registerAuthor);
 router.post('/login', authorController.loginAuthor);
 
 // Blog routes
-router.post('/blogs', authorAuth, blogController.createBlog);
-router.get('/blogs', authorAuth, blogController.listBlog);
-router.put('/blogs/:blogId', authorAuth, blogController.updateBlog);
-router.delete('/blogs/:blogId', authorAuth, blogController.deleteBlogByID);
-router.delete('/blogs', authorAuth, blogController.deleteBlogByParams);
+router.post('/blogs', Auth, blogController.createBlog);
+router.get('/blogs', Auth, blogController.listBlog);
+router.put('/blogs/:blogId', Auth, blogController.updateBlog);
+router.delete('/blogs/:blogId', Auth, blogController.deleteBlogByID);
+router.delete('/blogs', Auth, blogController.deleteBlogByParams);
 
 module.exports = router;
